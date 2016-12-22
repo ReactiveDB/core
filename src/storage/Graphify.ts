73e3a4a004d9180936c8d50db7c8d68bec34cb72
Saviio <jiaqi@teambition.com>
2016-12-22 18:03:13 +0800

    feat(Graphify): allow default method of `Graphify` module to remove un-queried field automatically

--- src/storage/Graphify.ts
+++ src/storage/Graphify.ts
@@ -1,10 +1,45 @@
 import { GRAPHIFY_ROWS_FAILED_ERR } from './RuntimeError'
+import { forEach } from '../utils'
+
 const nest = require('nesthydrationjs')().nest
 
-// primaryKey based, key: { id: true } should be given in definition at least.
+// filter the [[key]] with undefined
+function reduceUndef(obj: any) {
+  const ret = Array.isArray(obj) ? [] : Object.create(null)
+
+  forEach(obj, (val, key) => {
+    if (typeof val === 'object') {
+      if (val instanceof Date) {
+        ret[key] = val
+      } else if (Array.isArray(val)) {
+        const partial = val
+          .map((v) => reduceUndef(v))
+          .filter((v) => {
+            return !!v && Object.keys(v).length > 0
+          })
+
+        if (partial.length > 0 || (val.length === partial.length && val.length === 0)) {
+          // if both partial and val are empty array that means field was queried but no records matched
+          ret[key] = partial
+        }
+      } else {
+        ret[key] = val !== null ? reduceUndef(val) : null
+      }
+    } else {
+      if (val !== undefined) {
+        ret[key] = val
+      }
+    }
+  })
+
+  return ret
+}
+
+// primaryKey based, key: { id: true } must be given in definition.
 export default function<T>(rows: any[], definition: Object) {
   try {
-    return nest(rows, [definition]) as T[]
+    const result = nest(rows, [definition])
+    return reduceUndef(result) as T[]
   } catch (e) {
     throw GRAPHIFY_ROWS_FAILED_ERR(e)
   }
