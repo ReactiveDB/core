896c28814a5c3a54541d024d8d772bb8f42c7fb1
chuan6 <chuan6.dev@gmail.com>
2017-02-09 16:51:41 +0800

    refactor(Database & Graphify): 在 Graphify 中封装暴露在 Database 的
    ...与 nestJS 相关的操作。
    
    revert: remove Graphify.nestFieldName function
    
    ...since we are not using the semantics of double underscore of
    nestJS.
    
    Note, when a definition object is passed to nestJS.nest function,
    the semantics of double underscore in column alias is not used.

--- src/storage/Database.ts
+++ src/storage/Database.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs/Observable'
 import * as lf from 'lovefield'
 import { lfFactory } from './lovefield'
 import { RDBType, Association } from './DataType'
+import * as Graphify from './Graphify'
 import { Selector } from './Selector'
 import { QueryToken } from './QueryToken'
 import { PredicateDescription, PredicateProvider } from './PredicateProvider'
@@ -900,17 +901,12 @@ export class Database {
           columns.push(col)
           allFields[field] = true
 
-          const matcher: ShapeMatcher = {
-            column: fieldName,
-            id: !!schema[field].primaryKey
-          }
-
-          if (schema[field].type === RDBType.LITERAL_ARRAY) {
-            matcher.type = 'LiteralArray'
-          }
-
           if (!definition[field]) {
-            definition[field] = matcher
+            definition[field] = Graphify.definition(
+              fieldName,
+              !!schema[field].primaryKey,
+              schema[field].type
+            )
           } else {
             throw ALIAS_CONFLICT_ERR(field, tableName)
           }
