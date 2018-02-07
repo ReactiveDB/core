8bea7e5ea9a801faa618d268a7ec4cdcd9488054
Saviio <jiaqi@teambition.com>
2018-02-07 15:07:54 +0800

    fix(exception): make `exception` stack lazily

--- src/exception/Exception.ts
+++ src/exception/Exception.ts
@@ -1,25 +1,9 @@
-export interface ReactiveDBException extends Error { }
+export class ReactiveDBException extends Error {
 
-export interface ReactiveDBExceptionCtor {
-  new(message: string): ReactiveDBException
-  readonly prototype: ReactiveDBException
-}
-
-function ReactiveDBExceptionCtor(this: ReactiveDBException, message: string): ReactiveDBException {
-  const err = Error.call(this, message)
-  this.name = err.name
-  this.message = message
-  this.stack = err.stack
-  return this
-}
-
-ReactiveDBExceptionCtor.prototype = Object.create(Error.prototype, {
-  constructor: {
-    value: ReactiveDBExceptionCtor,
-    enumerable: false,
-    writable: true,
-    configurable: true
+  constructor(message: string) {
+    super(message)
+    this.name = 'ReactiveDBError';
+    (Object as any).setPrototypeOf(this, ReactiveDBException.prototype)
   }
-})
 
-export const ReactiveDBException = ReactiveDBExceptionCtor as any as ReactiveDBExceptionCtor
+}
