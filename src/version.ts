82c2dec6d88c945c93591557297230f40a6323e6
chuan6 <chuan6.dev@gmail.com>
2019-02-13 11:18:50 +0800

    0.10.3-alpha.3-lazyassert

--- src/version.ts
+++ src/version.ts
@@ -1,1 +1,1 @@
-export default '0.10.3-alpha.2-lazyassert-alpha0.10.3-alpha.2-lazyassert-lazyassert'
+export default '0.10.3-alpha.3-lazyassert-alpha0.10.3-alpha.3-lazyassert-lazyassert-alpha0.10.3-alpha.3-lazyassert-alpha0.10.3-alpha.3-lazyassert-lazyassert-lazyassert'
