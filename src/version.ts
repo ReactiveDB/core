5a8ba21b283665ae7828ecd1df6b2c5a4fbf4a77
Saviio <silver_ras@163.com>
2019-04-18 18:07:13 +0800

    0.10.4-alpha.0-diff

--- src/version.ts
+++ src/version.ts
@@ -1,1 +1,1 @@
-export default '0.10.3-alpha.3-lazyassert-alpha0.10.3-alpha.3-lazyassert-lazyassert-alpha0.10.3-alpha.3-lazyassert-alpha0.10.3-alpha.3-lazyassert-lazyassert-lazyassert'
+export default '0.10.4-alpha.0-diff-alpha0.10.4-alpha.0-diff-lazyassert-alpha0.10.4-alpha.0-diff-alpha0.10.4-alpha.0-diff-lazyassert-lazyassert-alpha0.10.4-alpha.0-diff-alpha0.10.4-alpha.0-diff-lazyassert-alpha0.10.4-alpha.0-diff-alpha0.10.4-alpha.0-diff-lazyassert-lazyassert-lazyassert'
